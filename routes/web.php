9e0af38d57e96b81de24a87ddc413323a86f8c14
Loctarogar <igrsl1990@gmail.com>
2019-03-14 23:20:47 +0300

    Add admin to auth system: routes for Forgot and Reset methods

--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,21 @@ Route::prefix('company')->name('company.')->group(function(){
 });
 
 Route::prefix('employee')->name('employee.')->group(function (){
-    Route::get('index', 'EmployeeController@index')->name('index');
+    Route::get('index',  'EmployeeController@index')->name('index');
     Route::get('create', 'EmployeeController@create')->name('create');
     Route::post('store', 'EmployeeController@store')->name('store');
-    Route::get('{employee}/edit', 'EmployeeController@edit')->name('edit');
-    Route::patch('update/{employee}', 'EmployeeController@update')->name('update');
+    Route::get('{employee}/edit',      'EmployeeController@edit')->name('edit');
+    Route::patch('update/{employee}',  'EmployeeController@update')->name('update');
     Route::delete('delete/{employee}', 'EmployeeController@destroy')->name('destroy');
 });
 
 Route::prefix('admin')->name('admin.')->group(function (){
-    Route::get('login', 'Auth\AdminLoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\AdminLoginController@login')->name('login.submit');
-    Route::get('dashboard', 'AdminController@index')->name('dashboard');
+    Route::get('login',           'Auth\AdminLoginController@showLoginForm')->name('login');
+    Route::post('login',          'Auth\AdminLoginController@login')->name('login.submit');
+    Route::get('dashboard',       'AdminController@index')->name('dashboard');
+
+    Route::post('password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset',  'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/reset', 'Auth\AdminResetPasswordController@reset')->name('password.update');
+    Route::get('password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('password.reset');
 });
