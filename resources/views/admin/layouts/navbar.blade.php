5812f53252c8af5205714d0f46dfeecbfcdf03ba
Loctarogar <igrsl1990@gmail.com>
2019-03-18 22:47:41 +0300

    Make Admin able to see changes sinse last enter: add migration last_enter

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -113,10 +113,11 @@
             </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-                <i class="fa fa-th-large"></i>
+            <a class="nav-link" data-widget="" data-slide="true" href="{{ route('admin.logout') }}">
+                <p>Log out</p>
             </a>
         </li>
+
     </ul>
 </nav>
 <!-- /.navbar -->
